c559b360b7ff7e841367bd19cbaa4422968c5711
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-01 23:05:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
-  <?php echo 'App Version: 0.4'; ?></br>
+  <?php echo 'App Version: 0.1'; ?></br>
   <?php echo ' Pod IP: ' . $_SERVER['SERVER_ADDR']; ?></br>
   <?php echo ' PHP Version: ' . phpversion(); ?></br>
   <?php echo ' Hostname: ' . gethostname(); ?></br>
