b5f8becccea14101a091e428ab3754424b383998
Steven Derycke <stevenderycke244@gmail.com>
2019-04-01 21:01:31 +0200

    initial upload

---
+++ health.php
@@ -0,0 +1,10 @@
+<?php
+function healthChecks() {
+    return "OK"
+}
+
+if CheckHealth() == "OK" {
+    http_response_code(200);
+} else {
+    http_response_code(500);
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+ </head>
+ <body>
+  <?php echo 'App Version: 0.3'; ?></br>
+  <?php echo ' Pod IP: ' . $_SERVER['SERVER_ADDR']; ?></br>
+  <?php echo ' PHP Version: ' . phpversion(); ?></br>
+ </body>
+</html>
