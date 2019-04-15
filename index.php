4c48b93b0f7c71a43a85a99493fdfc4993a6ca08
Steven Derycke <43333546+stevenderycke@users.noreply.github.com>
2019-04-15 10:02:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
-  <?php echo 'App Version: 0.1'; ?></br>
+  <?php echo 'App Version: 0.2'; ?></br>
   <?php echo 'Internal IP: ' . $_SERVER['SERVER_ADDR']; ?></br>
   <?php echo 'External IP: ' . file_get_contents('http://ifconfig.co/ip'); ?></br>
   <?php echo 'Hostname: ' . gethostname(); ?></br>
